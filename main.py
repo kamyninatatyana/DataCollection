787e568589208d2cf5c77646ec10bb60386a71a8
Tatyana <tatyanak@yandex.ru>
2021-04-14 18:39:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+import json
+from pprint import pprint
+
+# Задание 1: Посмотреть документацию к API GitHub, разобраться как вывести список репозиториев для
+# конкретного пользователя, сохранить JSON-вывод в файле *.json.
+
